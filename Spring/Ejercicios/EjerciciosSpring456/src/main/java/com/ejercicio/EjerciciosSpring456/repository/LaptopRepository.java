bcadaf3b21948b06da3462cfed9903b1a7805437
Daniel <112724243+iTzBigPerrito@users.noreply.github.com>
2022-09-08 15:43:13 -0500

    Delete Spring/Ejercicios/EjerciciosSpring456 directory

--- Spring/Ejercicios/EjerciciosSpring456/src/main/java/com/ejercicio/EjerciciosSpring456/repository/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ejercicio.EjerciciosSpring456.repository;
-
-import com.ejercicio.EjerciciosSpring456.entities.Laptop;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LaptopRepository extends JpaRepository<Laptop,Long> {
-}
